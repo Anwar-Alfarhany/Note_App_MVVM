d2a742256e482735479c9473e5f79c4e1605a54d
Anwar Alfarhany <anwar.alfarhany@gmail.com>
2021-11-26 17:00:15 +0300

    add companion object to note data base claa

--- app/src/main/java/com/anwaralfarhany/noteappmvvm/data/database/NoteDataBase.kt
+++ app/src/main/java/com/anwaralfarhany/noteappmvvm/data/database/NoteDataBase.kt
@@ -1,10 +1,29 @@
 package com.anwaralfarhany.noteappmvvm.data.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.anwaralfarhany.noteappmvvm.data.Note
 
 @Database(entities = [Note::class], version = 1)
 abstract class NoteDataBase : RoomDatabase() {
     abstract fun noteDao(): NoteDao
+
+    companion object {
+        private const val NOTE_DATABASE = "noteDataBase"
+
+        @Volatile
+        private var instance: NoteDataBase? = null
+
+        fun getInstance(context: Context): NoteDataBase {
+            return instance ?: synchronized(this) { buildDataBase(context).also { instance = it } }
+        }
+
+        fun getInstanceWithoutContext() = instance?.let { it }
+
+        private fun buildDataBase(context: Context): NoteDataBase {
+            return Room.databaseBuilder(context, NoteDataBase::class.java, NOTE_DATABASE).build()
+        }
+    }
 }
