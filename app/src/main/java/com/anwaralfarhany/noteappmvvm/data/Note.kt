e206d6b2673ac809216315d788748759114fd008
Anwar Alfarhany <anwar.alfarhany@gmail.com>
2021-11-26 15:51:59 +0300

    add entity note

---
+++ app/src/main/java/com/anwaralfarhany/noteappmvvm/data/Note.kt
@@ -0,0 +1,17 @@
+package com.anwaralfarhany.noteappmvvm.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "NOTE_TABLE")
+data class Note(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long,
+    @ColumnInfo(name= "Title")
+    val title: String,
+    @ColumnInfo(name= "Description")
+    val description: String,
+    @ColumnInfo(name= "Date")
+    val date: String,
+)
