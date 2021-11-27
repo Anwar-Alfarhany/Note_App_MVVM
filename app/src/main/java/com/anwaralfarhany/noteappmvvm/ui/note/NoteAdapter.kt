753daa1a7bd476040aaae8846c65a6f23793f7a0
Anwar Alfarhany <anwar.alfarhany@gmail.com>
2021-11-27 05:28:54 +0300

    add note adapter

---
+++ app/src/main/java/com/anwaralfarhany/noteappmvvm/ui/note/NoteAdapter.kt
@@ -0,0 +1,16 @@
+package com.anwaralfarhany.noteappmvvm.ui.note
+
+import com.anwaralfarhany.noteappmvvm.R
+import com.anwaralfarhany.noteappmvvm.data.Note
+import com.anwaralfarhany.noteappmvvm.ui.base.BaseAdapter
+import com.anwaralfarhany.noteappmvvm.ui.base.BaseInteractionListener
+import com.anwaralfarhany.noteappmvvm.ui.dialog.DialogListener
+
+class NoteAdapter(
+    items: List<Note>,
+    listener: NoteInteractionListener
+) : BaseAdapter<Note>(items, listener) {
+    override val layoutId: Int = R.layout.item_note
+}
+
+interface NoteInteractionListener : BaseInteractionListener
