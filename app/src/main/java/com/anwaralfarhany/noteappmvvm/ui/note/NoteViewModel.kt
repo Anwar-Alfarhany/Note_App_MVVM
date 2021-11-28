9d7bcf734e70b26b8d39e8c34ef4fa14f1f52e7c
Anwar Alfarhany <anwar.alfarhany@gmail.com>
2021-11-28 03:37:14 +0300

    add converters class

--- app/src/main/java/com/anwaralfarhany/noteappmvvm/ui/note/NoteViewModel.kt
+++ app/src/main/java/com/anwaralfarhany/noteappmvvm/ui/note/NoteViewModel.kt
@@ -1,6 +1,19 @@
 package com.anwaralfarhany.noteappmvvm.ui.note
 
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
+import com.anwaralfarhany.noteappmvvm.data.Note
+import com.anwaralfarhany.noteappmvvm.data.repository.NoteRepository
+import kotlinx.coroutines.launch
 
 class NoteViewModel : ViewModel(), NoteInteractionListener {
+    private val repository = NoteRepository()
+    private val noteTitle = MutableLiveData<String>()
+    private val noteDescription = MutableLiveData<String>()
+    val notes: LiveData<List<Note>> = repository.getAllNotes().asLiveData()
+
+    fun addNewNote() {
+        viewModelScope.launch {
+
+        }
+    }
 }
